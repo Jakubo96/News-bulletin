e1dfac3a9343ca9f654bdf4c4d134a1d44076c36
Jakub Woźny <jakubovip@gmail.com>
2020-01-05 12:37:29 +0100

    Add caching mechanism to firestore wrapper

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { News } from '@app/news/news';
 import { FirestoreCollections } from '@app/services/firestore/firestore-collections';
 import { Observable } from 'rxjs';
@@ -12,6 +12,9 @@ export class FirestoreService {
 
   private _newsCollection: AngularFirestoreCollection<News>;
   private _newsList: Observable<News[]>;
+
+  private _newsDocs = new Map<string, AngularFirestoreDocument<News>>();
+  private _newsItems = new Map<string, Observable<News>>();
 
   public get newsList() {
     return this._newsList;
@@ -34,13 +37,39 @@ export class FirestoreService {
   }
 
   public getNewsItem(id: string): Observable<News> {
+    if (this._newsItems.has(id)) {
+      return this._newsItems.get(id);
+    }
+
+    const {newsItem} = this.loadNewNews(id);
+
+
+    return newsItem;
+  }
+
+  public getNewsDoc(id: string): AngularFirestoreDocument<News> {
+    if (this._newsDocs.has(id)) {
+      return this._newsDocs.get(id);
+    }
+    const {newsDoc} = this.loadNewNews(id);
+
+    return newsDoc;
+  }
+
+  private loadNewNews(id: string): { newsDoc: AngularFirestoreDocument<News>, newsItem: Observable<News> } {
     const newsDoc = this.afs.doc<News>(`${FirestoreCollections.NEWS}/${id}`);
-    return newsDoc.snapshotChanges().pipe(
+
+    const newsItem = newsDoc.snapshotChanges().pipe(
       map(source => {
           const data = source.payload.data() as News;
 
           return {id, ...data};
         }
       ));
+
+    this._newsDocs.set(id, newsDoc);
+    this._newsItems.set(id, newsItem);
+
+    return {newsDoc, newsItem};
   }
 }
